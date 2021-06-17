128b5c9817c847672d0ed13047b4db91a777fd6b
yogianantaputra <yogi090891@gmail.com>
2021-06-17 13:01:36 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+from twilio.rest import Client
+
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+ALPHA_API_KEY = "PZF8668E5V0GZTX7"
+NEWS_API_KEY = "fb40908493a94ef6ae1212b79911a59c"
+
+parameters = {
+    "function" : "TIME_SERIES_DAILY",
+    "symbol" : STOCK, #TSLA
+    "apikey" : ALPHA_API_KEY,
+}
+
+response = requests.get(url="https://www.alphavantage.co/query?", params=parameters)
+stock_data = response.json()
+stock_daily = stock_data['Time Series (Daily)']
+
+stock_data_list = [value for (key, value) in stock_daily.items()]
+date = [x for x in stock_daily]
+
+#price
+yesterday_closing_price         = float(stock_data_list[0]['4. close'].replace(".", ""))
+day_bf_yesterday_closing_price  = float(stock_data_list[1]['4. close'].replace(".", ""))
+
+#difference price
+diff_percent = ((yesterday_closing_price - day_bf_yesterday_closing_price) / yesterday_closing_price) * 100
+
+print(f"closing_price_now :{yesterday_closing_price} | closing_price_ystrdy :{day_bf_yesterday_closing_price} |diff :{diff_percent}")
+print("\n")
+
+
+
+
+
+
